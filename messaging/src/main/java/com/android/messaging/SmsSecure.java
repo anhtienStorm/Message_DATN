8b7486e92d7542c5fb36b5a17b5060a52cb59f2f
anhtienStorm <anhtiennguyen1711@gmail.com>
2021-06-18 11:40:28 +0700

    Initial commit

---
+++ messaging/src/main/java/com/android/messaging/SmsSecure.java
@@ -0,0 +1,95 @@
+package com.android.messaging;
+
+import android.os.Build;
+
+import androidx.annotation.RequiresApi;
+
+import java.nio.charset.StandardCharsets;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.KeySpec;
+import java.util.Base64;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.SecretKey;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.PBEKeySpec;
+import javax.crypto.spec.SecretKeySpec;
+
+public class SmsSecure {
+
+    private static final String SALT = "thuuyennguyen";
+
+    private static IvParameterSpec ivspec;
+    private static SecretKeySpec secretKey;
+
+    public static void generateIV(){
+        byte[] iv = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+        ivspec = new IvParameterSpec(iv);
+    }
+
+    public static void generateSecretKey(String pass){
+        try {
+            SecretKeyFactory factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA256");
+            KeySpec spec = new PBEKeySpec(pass.toCharArray(), SALT.getBytes(), 65536, 256);
+            SecretKey tmp = factory.generateSecret(spec);
+            secretKey = new SecretKeySpec(tmp.getEncoded(), "AES");
+        } catch (NoSuchAlgorithmException | InvalidKeySpecException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    public static String encrypt(String strToEncrypt) {
+        try {
+            Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+            cipher.init(Cipher.ENCRYPT_MODE, secretKey, ivspec);
+            return Base64.getEncoder()
+                    .encodeToString(cipher.doFinal(strToEncrypt.getBytes(StandardCharsets.UTF_8)));
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        } catch (BadPaddingException e) {
+            e.printStackTrace();
+        } catch (InvalidKeyException e) {
+            e.printStackTrace();
+        } catch (InvalidAlgorithmParameterException e) {
+            e.printStackTrace();
+        } catch (NoSuchPaddingException e) {
+            e.printStackTrace();
+        } catch (IllegalBlockSizeException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    public static String decrypt(String strToDecrypt) {
+        try {
+            Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5PADDING");
+            cipher.init(Cipher.DECRYPT_MODE, secretKey, ivspec);
+            byte[] b = Base64.getDecoder().decode(strToDecrypt);
+            return new String(cipher.doFinal(b));
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        } catch (BadPaddingException e) {
+            e.printStackTrace();
+        } catch (InvalidKeyException e) {
+            e.printStackTrace();
+        } catch (InvalidAlgorithmParameterException e) {
+            e.printStackTrace();
+        } catch (NoSuchPaddingException e) {
+            e.printStackTrace();
+        } catch (IllegalBlockSizeException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
---
+++ messaging/src/main/java/com/android/messaging/ui/conversationlist/ConversationListActivity.java
@@ -0,0 +1,298 @@
+/*
+ * Copyright (C) 2015 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.messaging.ui.conversationlist;
+
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.ContentValues;
+import android.content.SharedPreferences;
+import android.database.Cursor;
+import android.graphics.drawable.ColorDrawable;
+import android.os.AsyncTask;
+import android.os.Build;
+import android.os.Bundle;
+
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AlertDialog;
+
+import android.provider.Telephony;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.messaging.R;
+import com.android.messaging.SmsSecure;
+import com.android.messaging.ui.UIIntents;
+import com.android.messaging.util.DebugUtils;
+import com.android.messaging.util.Trace;
+
+public class ConversationListActivity extends AbstractConversationListActivity {
+
+    private static final String MY_PREFS = "MyPrefs";
+    private static final String ENCRYPT = "Encrypt";
+    private static final String DECRYPT = "Decrypt";
+
+    SharedPreferences sharedPreferences;
+
+    @Override
+    protected void onCreate(final Bundle savedInstanceState) {
+        Trace.beginSection("ConversationListActivity.onCreate");
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.conversation_list_activity);
+        Trace.endSection();
+        invalidateActionBar();
+
+        sharedPreferences = getSharedPreferences(MY_PREFS, MODE_PRIVATE);
+    }
+
+    @Override
+    protected void updateActionBar(final ActionBar actionBar) {
+        actionBar.setTitle(getString(R.string.app_name));
+        actionBar.setDisplayShowTitleEnabled(true);
+        actionBar.setDisplayHomeAsUpEnabled(false);
+        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_STANDARD);
+        actionBar.setBackgroundDrawable(new ColorDrawable(
+                getResources().getColor(R.color.action_bar_background_color)));
+        actionBar.show();
+        super.updateActionBar(actionBar);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        // Invalidate the menu as items that are based on settings may have changed
+        // while not in the app (e.g. Talkback enabled/disable affects new conversation
+        // button)
+        supportInvalidateOptionsMenu();
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (isInConversationListSelectMode()) {
+            exitMultiSelectState();
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(final Menu menu) {
+        if (super.onCreateOptionsMenu(menu)) {
+            return true;
+        }
+        getMenuInflater().inflate(R.menu.conversation_list_fragment_menu, menu);
+        final MenuItem item = menu.findItem(R.id.action_debug_options);
+        if (item != null) {
+            final boolean enableDebugItems = DebugUtils.isDebugEnabled();
+            item.setVisible(enableDebugItems).setEnabled(enableDebugItems);
+        }
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(final MenuItem menuItem) {
+        int itemId = menuItem.getItemId();
+        if (itemId == R.id.action_start_new_conversation) {
+            onActionBarStartNewConversation();
+            return true;
+        } else if (itemId == R.id.action_settings) {
+            onActionBarSettings();
+            return true;
+        } else if (itemId == R.id.action_debug_options) {
+            onActionBarDebug();
+            return true;
+        } else if (itemId == R.id.action_show_archived) {
+            onActionBarArchived();
+            return true;
+        } else if (itemId == R.id.action_show_blocked_contacts) {
+            onActionBarBlockedParticipants();
+            return true;
+        } else if (itemId == R.id.action_encrypt) {
+            View inputPassEncryptLayout = getLayoutInflater().inflate(R.layout.input_password_layout, null);
+            EditText inputPassEncrypt = inputPassEncryptLayout.findViewById(R.id.input_password);
+            AlertDialog.Builder encryptDialogBuilder = new AlertDialog.Builder(this);
+            encryptDialogBuilder.setTitle("Nhập vào mật khẩu")
+                    .setView(inputPassEncryptLayout)
+                    .setPositiveButton("Ok", (dialogInterface, i) -> {
+                        new encryptDecryptAsyncTask(this).execute(
+                                ENCRYPT, inputPassEncrypt.getText().toString());
+                    })
+                    .setNegativeButton("Huỷ", (dialogInterface, i) -> {
+                    });
+            AlertDialog EncryptAlertDialog = encryptDialogBuilder.create();
+            EncryptAlertDialog.setCanceledOnTouchOutside(false);
+            EncryptAlertDialog.show();
+            return true;
+        } else if (itemId == R.id.action_decrypt) {
+            View inputPassDecryptLayout = getLayoutInflater().inflate(R.layout.input_password_layout, null);
+            EditText inputPassDecrypt = inputPassDecryptLayout.findViewById(R.id.input_password);
+            AlertDialog.Builder decryptDialogBuilder = new AlertDialog.Builder(this);
+            decryptDialogBuilder.setTitle("Nhập vào mật khẩu")
+                    .setView(inputPassDecryptLayout)
+                    .setPositiveButton("Ok", (dialogInterface, i) -> {
+                        new encryptDecryptAsyncTask(this).execute(
+                                DECRYPT, inputPassDecrypt.getText().toString());
+                    })
+                    .setNegativeButton("Huỷ", (dialogInterface, i) -> {
+                    });
+            AlertDialog DecryptAlertDialog = decryptDialogBuilder.create();
+            DecryptAlertDialog.setCanceledOnTouchOutside(false);
+            DecryptAlertDialog.show();
+            return true;
+        }
+        return super.onOptionsItemSelected(menuItem);
+    }
+
+    @Override
+    public void onActionBarHome() {
+        exitMultiSelectState();
+    }
+
+    public void onActionBarStartNewConversation() {
+        UIIntents.get().launchCreateNewConversationActivity(this, null);
+    }
+
+    public void onActionBarSettings() {
+        UIIntents.get().launchSettingsActivity(this);
+    }
+
+    public void onActionBarBlockedParticipants() {
+        UIIntents.get().launchBlockedParticipantsActivity(this);
+    }
+
+    public void onActionBarArchived() {
+        UIIntents.get().launchArchivedConversationsActivity(this);
+    }
+
+    @Override
+    public boolean isSwipeAnimatable() {
+        return !isInConversationListSelectMode();
+    }
+
+    @Override
+    public void onWindowFocusChanged(final boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        final ConversationListFragment conversationListFragment =
+                (ConversationListFragment) getFragmentManager().findFragmentById(
+                        R.id.conversation_list_fragment);
+        // When the screen is turned on, the last used activity gets resumed, but it gets
+        // window focus only after the lock screen is unlocked.
+        if (hasFocus && conversationListFragment != null) {
+            conversationListFragment.setScrolledToNewestConversationIfNeeded();
+        }
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    private void encryptAllMessage(String pass) {
+        SmsSecure.generateIV();
+        SmsSecure.generateSecretKey(pass);
+        Cursor cursor = getContentResolver().query(Telephony.Sms.CONTENT_URI, null,
+                null, null, null);
+        int d = 0;
+        while (cursor.moveToNext()) {
+            String body = cursor.getString(cursor.getColumnIndex(Telephony.Sms.BODY));
+            ContentValues values = new ContentValues();
+            values.put(Telephony.Sms.BODY, SmsSecure.encrypt("encrypted_by_AT" + body));
+            int numRowsUpdated = getContentResolver().update(Telephony.Sms.CONTENT_URI, values,
+                    Telephony.Sms._ID + "=?",
+                    new String[]{String.valueOf(cursor.getString(cursor.getColumnIndex(Telephony.Sms._ID)))});
+            d += numRowsUpdated;
+        }
+        Log.d("TienNAb", "updateMessage: " + d);
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    private void decryptAllMessage(String pass) {
+        SmsSecure.generateIV();
+        SmsSecure.generateSecretKey(pass);
+        Cursor cursor = getContentResolver().query(Telephony.Sms.CONTENT_URI, null,
+                null, null, null);
+        int d = 0;
+        while (cursor.moveToNext()) {
+            String body = cursor.getString(cursor.getColumnIndex(Telephony.Sms.BODY));
+            String decryptBody = SmsSecure.decrypt(body);
+            ContentValues values = new ContentValues();
+            if (decryptBody == null){
+                values.put(Telephony.Sms.BODY, body);
+            } else if (decryptBody.contains("encrypted_by_AT")) {
+                values.put(Telephony.Sms.BODY, decryptBody.replace("encrypted_by_AT", ""));
+            } else {
+                values.put(Telephony.Sms.BODY, body);
+            }
+            int numRowsUpdated = getContentResolver().update(Telephony.Sms.CONTENT_URI, values,
+                    Telephony.Sms._ID + "=?",
+                    new String[]{String.valueOf(cursor.getString(cursor.getColumnIndex(Telephony.Sms._ID)))});
+            d += numRowsUpdated;
+        }
+        Log.d("TienNAb", "updateMessage: " + d);
+    }
+
+    private class encryptDecryptAsyncTask extends AsyncTask<String, String, Void> {
+
+        Activity activity;
+        ProgressDialog progressDialog;
+
+        public encryptDecryptAsyncTask(Activity activity) {
+            this.activity = activity;
+        }
+
+        @Override
+        protected Void doInBackground(String... strings) {
+            publishProgress(strings[0]);
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                if (ENCRYPT.equals(strings[0])) {
+//                    encryptAllMessage(strings[1]);
+                    SharedPreferences.Editor editor = sharedPreferences.edit();
+                    editor.putBoolean(ENCRYPT, true);
+                    editor.apply();
+                } else if (DECRYPT.equals(strings[0])) {
+//                    decryptAllMessage(strings[1]);
+                    SharedPreferences.Editor editor = sharedPreferences.edit();
+                    editor.putBoolean(ENCRYPT, false);
+                    editor.apply();
+                }
+            }
+            return null;
+        }
+
+        @Override
+        protected void onProgressUpdate(String... values) {
+            super.onProgressUpdate(values);
+            progressDialog = new ProgressDialog(activity);
+            progressDialog.show();
+            View view = activity.getLayoutInflater().inflate(R.layout.progress_dialog_layout, null);
+            TextView textLoading = view.findViewById(R.id.text_loading);
+            textLoading.setText(values[0] + "ing...");
+            progressDialog.setContentView(view);
+            progressDialog.getWindow().setBackgroundDrawableResource(android.R.color.transparent);
+            progressDialog.setCanceledOnTouchOutside(false);
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+            progressDialog.dismiss();
+            Toast.makeText(activity, "Done", Toast.LENGTH_SHORT).show();
+//            load_list_message();
+        }
+    }
+}
